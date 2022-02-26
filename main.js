e18ba0547ddfd4d84288fb9c01c3f1d7d33970b6
Enock <Enock@DESKTOP-862AS8S>
2022-02-26 14:18:15 +0300

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load' , () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const input = document.querySelector("#new-task-input");
+     
+})
